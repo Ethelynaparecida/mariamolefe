13eee0217428ef2883ce6e2ffc969f67c679645e
Ethelynaparecida <ethelynaparecida@gmail.com>
2025-11-14 21:17:10 -0300

    novas funcoes adm, lista de usuarios, limpeza de banco

--- src/app/service/auth.guard.ts
+++ src/app/service/auth.guard.ts
@@ -7,11 +7,20 @@ export const authGuard: CanActivateFn = (route, state) => {
   const loginService = inject(LoginService);
   const router = inject(Router);
 
-  if (loginService.estaLogado()) {
-    return true; 
+  if (!loginService.estaLogado()) {
+    console.warn('AuthGuard: Utilizador não logado. A redirecionar para /login');
+    router.navigate(['/login']);
+    return false;
   }
 
-  console.warn('Acesso bloqueado pela AuthGuard. A redirecionar para /login');
-  router.navigate(['/login']);
-  return false;
+  if (state.url.includes('/fila')) {
+    if (loginService.estaNaFila()) {
+      return true;
+    } else {
+      console.warn("AuthGuard: Utilizador logado mas sem música na fila. A redirecionar de /fila para /buscar.");
+      router.navigate(['/buscar']);
+      return false;
+    }
+  }
+  return true;
 };
